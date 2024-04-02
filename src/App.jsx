95048355af26e00421f3c6451bfc5c76c11b099b
Yibo <yibo@Yibo.yibo>
2024-04-01 21:28:56 -0400

    123

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edi12321312t <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
