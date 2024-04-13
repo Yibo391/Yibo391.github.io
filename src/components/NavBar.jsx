cebf3a61c8d01aca979549c22978457ce223205f
Yibo391 <391583391@qq.com>
2024-04-13 22:35:56 +0200

    make the switcher button in a proper position

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next'; // Import the hook
+import { useTranslation } from 'react-i18next';
 import '../styles/NavBar.css';
-import LanguageSwitcher from './LanguageSwitcher'; // Make sure the path is correct
+import LanguageSwitcher from './LanguageSwitcher';
 
 const NavBar = () => {
-    const { t } = useTranslation(); // Initialize the translation hook
-  
+    const { t } = useTranslation();
+    const [isOpen, setIsOpen] = useState(false);  // State to manage the collapse
+
+    const toggle = () => setIsOpen(!isOpen); // Toggle the state
+
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">{t('home')}</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" onClick={toggle} aria-controls="navbarNav" aria-expanded={isOpen} aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link" to="/weather">{t('weather')}</Link>
+                <Link className="nav-link" to="/" onClick={() => setIsOpen(false)}>{t('home')}</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/photo">{t('photos')}</Link>
+                <Link className="nav-link" to="/weather" onClick={() => setIsOpen(false)}>{t('weather')}</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/todo">{t('todoList')}</Link>
+                <Link className="nav-link" to="/photo" onClick={() => setIsOpen(false)}>{t('photos')}</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/calculator">{t('calculator')}</Link>
+                <Link className="nav-link" to="/todo" onClick={() => setIsOpen(false)}>{t('todoList')}</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/calculator" onClick={() => setIsOpen(false)}>{t('calculator')}</Link>
               </li>
             </ul>
-            <div className="ms-auto">
-              <LanguageSwitcher />  {/* Include the LanguageSwitcher in the navbar on the right side */}
-            </div>
+          </div>
+          <div className="ms-auto">
+            <LanguageSwitcher />
           </div>
         </div>
       </nav>
